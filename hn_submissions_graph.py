1788b9db366410583120a50de5d859b47542eafc
Connor Foster <foster.clee@gmail.com>
2019-05-13 21:43:02 -0500

    Fixed x label ticks, were in the correct order, made a new list after sorting

--- hn_submissions_graph.py
+++ hn_submissions_graph.py
@@ -12,7 +12,7 @@ print("Status Code: " + str(r.status_code))
 # Process informtion about each submission
 
 submission_ids = r.json()
-submission_dicts, titles = [], []
+submission_dicts = []
 for submission in submission_ids[:30]:
     # Make a separate API call for each submission
     url = ('https://hacker-news.firebaseio.com/v0/item/' + 
@@ -26,17 +26,18 @@ for submission in submission_ids[:30]:
     'link':('htt://news.ycombinator.com/item?id-' + str(submission)),
     'value':response_dict.get('descendants', 0)
     }
-    print(response_dict['descendants'])
     submission_dicts.append(submission_dict)
-    titles.append(response_dict['title'])
+
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('value'),
     reverse=True)
-
+    
+titles = []
 for submission_dict in submission_dicts:
     print("\nTitle:", submission_dict['title'])
     print("Discussion link:", submission_dict['link'])
     print("Comments:", submission_dict['value'])
+    titles.append(submission_dict['title'])
     
 chart = pygal.Bar(x_label_rotation=90)
 chart.add('Articles', submission_dicts)
